265fe8580422fa51bb7dc3b0e4e7c4dbb71813c8
Hamsavardhan <hamsavardhanom@gmail.com>
2026-02-05 20:59:19 +0530

    Add configure basic index and app files and test the working with postman

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log("This is the server");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: "true", limit: "16kb"}));
+app.use(express.static("public"));
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"]
+}));
+
+app.use(cookieParser());
+
+//import routes
+
+//Endpoints configuration
+
+app.get("/",(req,res) =>
+{
+    res.send("App is working fine!");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv"
+import app from "./app.js";
 
 dotenv.config({
     path: "./.env"
 });
 
-console.log(process.env.test);
-console.log("test");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+            console.log(
+                `Example app listening on port https://localhost:${port}`,
+            );
+        });
