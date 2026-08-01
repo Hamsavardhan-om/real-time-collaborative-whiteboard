cedf4d43a5d727f814d5e8d5e1c1eea05b1aaaa5
Hamsavardhan <hamsavardhanom@gmail.com>
2026-08-01 14:56:05 +0530

    fix small bug in controller, now it allows secure as well as non-secure plain http requests also

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -105,7 +105,8 @@ const loginUser = asyncHandler(async (req, res) =>
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: false,
+        sameSite: "lax"
     }
 
     return res
